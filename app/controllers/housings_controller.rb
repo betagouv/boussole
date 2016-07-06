eef60fc146f682ce2ae530aea037bc5c70d9d426
Mauko Quiroga <maukoquiroga@gmail.com>
2016-07-06 19:20:11 +0200

    Results page draft I

--- app/controllers/housings_controller.rb
+++ app/controllers/housings_controller.rb
@@ -8,16 +8,20 @@ class HousingsController < ApplicationController
   def show
     @housing = Housing.find(params[:id])
 
-    @results = { a: [], b: [] }
+    public_services = YAML.load(File.read(Rails.root.join('config', 'parameters.yml')))['public_services']
+    mesures         = YAML.load(File.read(Rails.root.join('config', 'parameters.yml')))['mesures']
 
-    @results[:a] << 'SIAO'          if @housing.siao?
-    @results[:a] << 'Crous'         if @housing.crous?
-    @results[:a] << "Pain d'avoine" if @housing.pain_d_avoine?
-    @results[:b] << '115'           if @housing.cent_quinze?
-    @results[:b] << 'APL'           if @housing.apl?
-    @results[:b] << 'CLE'           if @housing.cle?
-    @results[:b] << 'Locapass'      if @housing.locapass?
-    @results[:b] << 'Visale'        if @housing.visale?
+    @results = { public_services: [], mesures: [] }
+
+    @results[:public_services] << public_services['siao']          if @housing.siao?
+    @results[:public_services] << public_services['crous']         if @housing.crous?
+    @results[:public_services] << public_services['pain_d_avoine'] if @housing.pain_d_avoine?
+
+    @results[:mesures] << mesures['cent_quinze'] if @housing.cent_quinze?
+    @results[:mesures] << mesures['apl']         if @housing.apl?
+    @results[:mesures] << mesures['cle']         if @housing.cle?
+    @results[:mesures] << mesures['locapass']    if @housing.locapass?
+    @results[:mesures] << mesures['visale']      if @housing.visale?
   end
 
   # POST /projects
