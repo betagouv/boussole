48feae796b50bbc5a728eb758e94a401b045ae12
Mauko Quiroga <maukoquiroga@gmail.com>
2016-07-07 18:21:12 +0200

    Results page draft II

--- app/controllers/housings_controller.rb
+++ app/controllers/housings_controller.rb
@@ -11,17 +11,40 @@ class HousingsController < ApplicationController
     public_services = YAML.load(File.read(Rails.root.join('config', 'parameters.yml')))['public_services']
     mesures         = YAML.load(File.read(Rails.root.join('config', 'parameters.yml')))['mesures']
 
-    @results = { public_services: [], mesures: [] }
+    @results = []
 
-    @results[:public_services] << public_services['siao']          if @housing.siao?
-    @results[:public_services] << public_services['crous']         if @housing.crous?
-    @results[:public_services] << public_services['pain_d_avoine'] if @housing.pain_d_avoine?
+    # SIAO
+    if @housing.siao? || @housing.cent_quinze?
+      public_services['siao'][:mesures] = []
+      public_services['siao'][:mesures] << mesures['cent_quinze'] if @housing.cent_quinze?
+      @results << public_services['siao']
+    end
 
-    @results[:mesures] << mesures['cent_quinze'] if @housing.cent_quinze?
-    @results[:mesures] << mesures['apl']         if @housing.apl?
-    @results[:mesures] << mesures['cle']         if @housing.cle?
-    @results[:mesures] << mesures['locapass']    if @housing.locapass?
-    @results[:mesures] << mesures['visale']      if @housing.visale?
+    # Crous
+    if @housing.crous? || @housing.cle?
+      public_services['crous'][:mesures] = []
+      public_services['crous'][:mesures] << mesures['cle'] if @housing.cle?
+      @results << public_services['crous']
+    end
+
+    # Paindavoine
+    if @housing.pain_d_avoine?
+      public_services['pain_d_avoine'][:mesures] = []
+      @results << public_services['pain_d_avoine']
+    end
+
+    if @housing.locapass? || @housing.visale?
+      public_services['action_logement'][:mesures] = []
+      public_services['action_logement'][:mesures] << mesures['locapass'] if @housing.locapass?
+      public_services['action_logement'][:mesures] << mesures['visale']   if @housing.visale?
+      @results << public_services['action_logement']
+    end
+
+    # CAF
+    # if @housing.apl?
+    #   public_services['caf'][:mesures] = [mesures['apl']]
+    #   @results << public_services['caf']
+    # end
   end
 
   # POST /projects
