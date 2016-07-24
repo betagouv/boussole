c943301e76400af0cde3e615a24aecc26e8d807f
Mauko Quiroga <maukoquiroga@gmail.com>
2016-07-24 19:44:27 +0200

    Rename mesure to measure

--- app/controllers/housings_controller.rb
+++ app/controllers/housings_controller.rb
@@ -28,40 +28,40 @@ class HousingsController < ApplicationController
 
   def load_results_for_siao
     if @housing.siao? || @housing.cent_quinze?
-      public_services['siao'][:mesures] = []
-      public_services['siao'][:mesures] << mesures['cent_quinze'] if @housing.cent_quinze?
+      public_services['siao'][:measures] = []
+      public_services['siao'][:measures] << measures['cent_quinze'] if @housing.cent_quinze?
       @results << public_services['siao']
     end
   end
 
   def load_results_for_crous
     if @housing.crous? || @housing.cle?
-      public_services['crous'][:mesures] = []
-      public_services['crous'][:mesures] << mesures['cle'] if @housing.cle?
+      public_services['crous'][:measures] = []
+      public_services['crous'][:measures] << measures['cle'] if @housing.cle?
       @results << public_services['crous']
     end
   end
 
   def load_results_for_paindavoine
     if @housing.paindavoine?
-      public_services['paindavoine'][:mesures] = []
+      public_services['paindavoine'][:measures] = []
       @results << public_services['paindavoine']
     end
   end
 
   def load_results_for_action_logement
     if @housing.locapass? || @housing.visale?
-      public_services['action_logement'][:mesures] = []
-      public_services['action_logement'][:mesures] << mesures['locapass'] if @housing.locapass?
-      public_services['action_logement'][:mesures] << mesures['visale']   if @housing.visale?
+      public_services['action_logement'][:measures] = []
+      public_services['action_logement'][:measures] << measures['locapass'] if @housing.locapass?
+      public_services['action_logement'][:measures] << measures['visale']   if @housing.visale?
       @results << public_services['action_logement']
     end
   end
 
   def load_results_for_caf
     if @housing.apl?
-      public_services['caf'][:mesures] = []
-      public_services['caf'][:mesures] << mesures['apl']
+      public_services['caf'][:measures] = []
+      public_services['caf'][:measures] << measures['apl']
       @results << public_services['caf']
     end
   end
@@ -70,7 +70,7 @@ class HousingsController < ApplicationController
     @public_services ||= YAML.load(File.read(Rails.root.join('config', 'parameters.yml')))['public_services']
   end
 
-  def mesures
-    @mesures = YAML.load(File.read(Rails.root.join('config', 'parameters.yml')))['mesures']
+  def measures
+    @measures = YAML.load(File.read(Rails.root.join('config', 'parameters.yml')))['measures']
   end
 end
