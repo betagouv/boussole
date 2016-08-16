d493c4bae10aed56feafcfe9c38a8c0b5f168e0c
Mauko Quiroga <maukoquiroga@gmail.com>
2016-08-16 18:27:07 +0200

    Show current sha in prod

--- config/initializers/release_revision.rb
+++ config/initializers/release_revision.rb
@@ -1,4 +1,16 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-REVISION = `git log --pretty=format:'%h' -n 1`.freeze
+revision =
+  # Capistrano & friends
+  if File.exist?(Rails.root.join('REVISION'))
+    `cat #{Rails.root.join('REVISION')}`
+  # Heroku
+  elsif File.exist?(Rails.root.join('.source_version'))
+    `cat #{Rails.root.join('.source_version')}`
+  # Whatever else
+  else
+    `git rev-parse HEAD`
+  end
+
+REVISION = revision.chomp.send(:[], 0..10).freeze
