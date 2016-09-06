4dd6ecf409614fcbe128d65349cd91025bcf71b4
Mauko Quiroga <maukoquiroga@gmail.com>
2016-09-06 22:01:42 +0200

    ServiceOffering matching

--- spec/models/housing_spec.rb
+++ spec/models/housing_spec.rb
@@ -22,212 +22,117 @@ RSpec.describe Housing, type: :model do
     end
   end
 
-  describe '#siao?' do
-    context 'for non urgences' do
-      it { expect(build(:housing).siao?).to eq(false) }
+  describe '#match' do
+    let(:public_service)   { create(:public_service, title: 'SIAO') }
+    let(:service_offering) { create(:service_offering, title: 'Solution logement urgence', public_service: public_service) }
+
+    let(:housing) do
+      create(
+        :housing,
+        duration: '1 an',
+        housing_city: 'Reims',
+        current_status: 'Étudiant·e',
+        resources: 150
+      )
     end
 
-    context 'for people with enough resources' do
-      let(:housing) { build(:housing, resources: 301) }
-
-      it { expect(housing.siao?).to eq(false) }
-    end
-
-    context 'for urgences and without enough resources' do
-      let(:housing) { build(:housing, duration: 'Cette nuit', resources: 300) }
-
-      it { expect(housing.siao?).to eq(true) }
-    end
-  end
-
-  describe '#crous?' do
-    context 'for more than a year' do
-      let(:housing) { build(:housing, duration: "+ d'1 an") }
-
-      it { expect(housing.crous?).to eq(false) }
-    end
-
-    context 'for a non student' do
-      it { expect(build(:housing).crous?).to eq(false) }
-    end
-
-    context 'for people with enough resources' do
-      let(:housing) { build(:housing, duration: 'Quelques mois', current_status: 'Étudiant·e', resources: 1_201) }
-
-      it { expect(housing.crous?).to eq(false) }
-    end
-
-    context 'for students without resources and for no more than a year' do
-      let(:housing) { build(:housing, duration: 'Quelques mois', current_status: 'Étudiant·e', resources: 1_200) }
-
-      it { expect(housing.crous?).to eq(true) }
-    end
-
-    context 'for students without resources and for a year' do
-      let(:housing) { build(:housing, duration: '1 an', current_status: 'Étudiant·e', resources: 1_200) }
-
-      it { expect(housing.crous?).to eq(true) }
-    end
-  end
-
-  describe '#paindavoine?' do
-    context 'for urgences' do
-      let(:housing) { build(:housing, duration: 'Cette nuit') }
-
-      it { expect(housing.paindavoine?).to eq(false) }
-    end
-
-    context 'for students' do
-      let(:housing) { build(:housing, current_status: 'Étudiant·e') }
-
-      it { expect(housing.paindavoine?).to eq(false) }
-    end
-
-    context 'for people without enough resources' do
-      let(:housing) { build(:housing, resources: 299) }
-
-      it { expect(housing.paindavoine?).to eq(false) }
-    end
-
-    context 'for non urgences, highschooler and with enough resources' do
-      let(:housing) { build(:housing, current_status: 'Lycéen·ne', resources: 300) }
-
-      it { expect(housing.paindavoine?).to eq(false) }
-    end
-
-    context 'for non urgences, students and with enough resources' do
-      let(:housing) { build(:housing, current_status: 'Lycéen·ne', next_status: true, resources: 300) }
-
-      it { expect(housing.paindavoine?).to eq(true) }
-    end
-  end
-
-  describe '#cent_quinze?' do
-    context 'for non urgences' do
-      it { expect(build(:housing).cent_quinze?).to eq(false) }
-    end
-
-    context 'for people with enough resources' do
-      let(:housing) { build(:housing, resources: 1_001) }
-
-      it { expect(housing.cent_quinze?).to eq(false) }
-    end
-
-    context 'for really young people' do
-      let(:housing) { build(:housing, age: 15) }
-
-      it { expect(housing.cent_quinze?).to eq(false) }
-    end
-
-    context 'for not-so-young adults' do
-      let(:housing) { build(:housing, age: 31) }
-
-      it { expect(housing.cent_quinze?).to eq(false) }
-    end
-
-    context 'for urgences, for young people without enough resources' do
-      let(:housing) do
-        build(
-          :housing,
-          duration: 'Cette nuit',
-          resources: 1_000,
-          age: 30
-        )
+    before do
+      allow(housing).to receive(:rules) do
+        {
+          'siao' => {
+            'solution-logement-urgence' => [
+              [
+                {
+                  'attribute' => 'duration',
+                  'operator' => 'in?',
+                  'value' => ['Quelques mois', '1 an', "+ d'1 an"]
+                },
+                {
+                  'attribute' => 'resources',
+                  'operator' => '>=',
+                  'value' => 150
+                },
+                {
+                  'attribute' => 'current_status',
+                  'operator' => 'in?',
+                  'value' => ['Étudiant·e', 'Lycéen·ne', 'Salarié·e', 'En formation', 'Apprenti·e']
+                }
+              ]
+            ]
+          }
+        }
       end
+    end
 
-      it { expect(housing.cent_quinze?).to eq(true) }
+    context 'with matches' do
+      it { expect(housing.matches).to include(service_offering) }
+    end
+
+    context 'without matches' do
+      before { housing.update_attributes(resources: 0) }
+
+      it { expect(housing.matches).to be_empty }
     end
   end
 
-  describe '#apl?' do
-    context 'for urgences' do
-      let(:housing) { build(:housing, duration: 'Cette nuit') }
+  # TODO: Refacto to rules object
+  # TODO: Use RSpec hash/custom matchers
+  describe '#rules' do
+    rules = YAML.load(File.read(Rails.root.join('config', 'rules', 'reims', 'service_offerings.yml')))
 
-      it { expect(housing.apl?).to eq(false) }
+    rules.each do |public_service_key, public_service|
+      it { expect(public_service_key).to match(/\A[a-z0-9\-]+\z/) }
+
+      public_service.each do |service_offering_key, scenarios|
+        it { expect(service_offering_key).to match(/\A[a-z0-9\-]+\z/) }
+
+        scenarios.each do |conditions|
+          conditions.each do |condition|
+            # Valid keys
+            it { expect(condition.keys).to eq(%w(attribute operator value)) }
+
+            # TODO: Define a (custom) compound matcher for this
+            # Whether the attribute is a valid housing attribute
+            it { expect(described_class.column_names).to include(condition['attribute']) }
+
+            # TODO: Define a (custom) compound matcher for this
+            # Permitted operators
+            it { expect(%w(in? == != < <= >= >)).to include(condition['operator']) }
+
+            # Value should either be an array, a string or a number
+            it { expect(condition['value']).to be_a_kind_of(Array) | be_a_kind_of(String) | be_a_kind_of(Integer) }
+
+            case condition['attribute']
+            when 'duration'
+              # Whether the duration value/s are valid or not
+              # TODO: Define a custom matcher!
+              it { expect(Array.wrap(condition['value']).to_set.subset?(DURATIONS.to_set)).to be(true) }
+              it { expect(%w(in? == !=)).to include(condition['operator']) }
+              it { expect(condition['value']).to be_a_kind_of(Array) } if condition['operator'] == 'in?'
+              it { expect(condition['value']).to be_a_kind_of(String) } if condition['operator'].in?(%w(== !=))
+
+            when 'current_status'
+              # Whether the current_status value/s are valid or not
+              # TODO: Define a custom matcher!
+              it { expect(Array.wrap(condition['value']).to_set.subset?(STATUSES.to_set)).to be(true) }
+              it { expect(%w(in? ==)).to include(condition['operator']) }
+              it { expect(condition['value']).to be_a_kind_of(Array) } if condition['operator'] == 'in?'
+              it { expect(condition['value']).to be_a_kind_of(String) } if condition['operator'].in?(%w(== !=))
+
+            when /resources|age/
+              # Whether the resources or age value/s are valid or not
+              it { expect(%w(in?)).not_to include(condition['operator']) }
+              it { expect(condition['value']).to be_a_kind_of(Integer) }
+
+            when /housing_city|residence_city/
+              it { expect(%w(== !=)).to include(condition['operator']) }
+
+            when 'next_status'
+              it { expect(condition['value']).to be(true) }
+            end
+          end
+        end
+      end
     end
-
-    context 'for non urgences' do
-      it { expect(build(:housing).apl?).to eq(true) }
-    end
-  end
-
-  describe '#cle?' do
-    context 'for urgences' do
-      let(:housing) { build(:housing, duration: 'Cette nuit', current_status: 'Étudiant·e') }
-
-      it { expect(housing.cle?).to eq(false) }
-    end
-
-    context 'for non students' do
-      it { expect(build(:housing).cle?).to eq(false) }
-    end
-
-    context 'for non urgences' do
-      let(:housing) { build(:housing, current_status: 'Étudiant·e') }
-
-      it { expect(housing.cle?).to eq(true) }
-    end
-  end
-
-  describe '#locapass?' do
-    context 'for urgences' do
-      let(:housing) { build(:housing, duration: 'Cette nuit', current_status: 'Salarié·e') }
-
-      it { expect(housing.locapass?).to eq(false) }
-    end
-
-    context 'for not-so-young adults' do
-      let(:housing) { build(:housing, age: 30) }
-
-      it { expect(housing.locapass?).to eq(false) }
-    end
-
-    context 'for employees' do
-      let(:housing) { build(:housing, current_status: 'Salarié·e') }
-
-      it { expect(housing.locapass?).to eq(true) }
-    end
-
-    context "for young people undertaking an 'alternance'" do
-      let(:housing) { build(:housing, current_status: 'En alternance', age: 29) }
-
-      it { expect(housing.locapass?).to eq(true) }
-    end
-
-    context 'for young job seekers' do
-      let(:housing) { build(:housing, current_status: 'Sans activité', age: 29) }
-
-      it { expect(housing.locapass?).to eq(true) }
-    end
-  end
-
-  describe '#visale?' do
-    context 'for less than a year' do
-      let(:housing) { build(:housing, duration: 'Quelques mois') }
-
-      it { expect(housing.visale?).to eq(false) }
-    end
-
-    context 'for non employees' do
-      it { expect(build(:housing).visale?).to eq(false) }
-    end
-
-    context 'for a year and for employees' do
-      let(:housing) { build(:housing, duration: '1 an', current_status: 'Salarié·e') }
-
-      it { expect(housing.visale?).to eq(true) }
-    end
-
-    context 'for more than a year and for employees' do
-      let(:housing) { build(:housing, duration: "+ d'1 an", current_status: 'Salarié·e') }
-
-      it { expect(housing.visale?).to eq(true) }
-    end
-  end
-
-  describe '#student?' do
-    it { expect(build(:housing, current_status: 'Étudiant·e', next_status: false).student?).to eq(true) }
-    it { expect(build(:housing, current_status: 'Lycéen·ne', next_status: true).student?).to eq(true) }
-    it { expect(build(:housing, next_status: true).student?).to eq(false) }
   end
 end
