86a57f3acce5538fd74f4d5fc662e7fb0f36dd1e
Mauko Quiroga <maukoquiroga@gmail.com>
2016-09-19 15:26:30 +0200

    Sentry: do not exlude errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,8 @@
 # frozen_string_literal: true
 
 Raven.configure do |config|
-  config.dsn             = ENV['SENTRY_DSN']
-  config.environments    = %w(production)
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.dsn                 = ENV['SENTRY_DSN']
+  config.environments        = %w(production)
+  config.excluded_exceptions = []
+  config.sanitize_fields     = Rails.application.config.filter_parameters.map(&:to_s)
 end
