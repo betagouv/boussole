4a18981dc4d30e2f2c0d873fcd88be869a30928c
Mauko Quiroga <maukoquiroga@gmail.com>
2016-09-22 11:12:39 +0200

    Fix Sentry configuration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 Raven.configure do |config|
-  config.dsn                 = ENV['SENTRY_DSN']
+  config.dsn                 = ENV['SENTRY_PRIVATE_DSN']
   config.environments        = %w(production)
   config.excluded_exceptions = []
   config.sanitize_fields     = Rails.application.config.filter_parameters.map(&:to_s)
