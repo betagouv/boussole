53819e72affdef09e7ec4c29d735c776098cb15c
Mauko Quiroga <maukoquiroga@gmail.com>
2016-09-26 02:30:47 +0200

    Route flyer URLs to landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,28 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :agents do
-    root to: 'dashboard#index'
+  namespace(:agents) do
+    root(to: 'dashboard#index')
 
-    resources :public_services
-    resources :service_offerings
-    resources :measures
+    resources(:public_services)
+    resources(:service_offerings)
+    resources(:measures)
   end
 
-  resources :projects, only: %i(show create) do
-    resources :steps, only: %i(show update), module: :projects
+  resources(:projects, only: %i(show create)) do
+    resources(:steps, only: %i(show update), module: :projects)
   end
 
-  resources :housings, only: %i(show create) do
-    resources :steps, only: %i(show update), module: :housings
-    resources :service_offerings, only: :show, module: :housings
+  resources(:housings, only: %i(show create)) do
+    resources(:steps, only: %i(show update), module: :housings)
+    resources(:service_offerings, only: :show, module: :housings)
   end
 
-  resources :contacts, only: %i(show create)
+  resources(:contacts, only: %i(show create))
 
-  mount Flip::Engine => '/flip'
+  mount(Flip::Engine => '/flip')
+
+  get '/:flyer', to: 'visitors#index', constraints: { flyer: /reims1[A|B|C|D|E|F|G|H|J|K|L|N]/ }
 
   root to: 'visitors#index'
 end
