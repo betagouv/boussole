fc6b1366681d230b3c2c36f97f8b9222a394934a
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2016-11-03 18:04:56 +0100

    Décore offres de service
    In order to handle the fallback case of a ServiceOffering not having
    defined a response time.

--- app/controllers/workings/service_offerings_controller.rb
+++ app/controllers/workings/service_offerings_controller.rb
@@ -7,10 +7,58 @@ module Workings
 
     # GET /workings/1/service_offerings/1
     def show
-      @working          = Working.find(params[:working_id])
-      @service_offering = ServiceOffering.friendly.find(params[:id])
-      @public_service   = @service_offering.public_service
-      @contact          = Contact.new
+      load_working
+      load_service_offering
+      load_public_service
+      build_contact
+      decorate_service_offering
+    end
+
+    private
+
+    def load_working
+      @working ||= working_scope.find(working_params)
+    end
+
+    def load_service_offering
+      @service_offering ||= service_offering_scope.find(service_offering_params)
+    end
+
+    def load_public_service
+      @public_service ||= @service_offering.public_service
+    end
+
+    def build_contact
+      @contact ||= contact_scope.build
+      @contact.attributes = contact_params
+    end
+
+    def decorate_service_offering
+      @service_offering = ServiceOfferingDecorator.(@service_offering)
+    end
+
+    def working_params
+      params[:working_id]
+    end
+
+    def service_offering_params
+      params[:id]
+    end
+
+    def contact_params
+      params[:contact] ? params.require(:contact).permit(email_or_phone: Parameters.string) : {}
+    end
+
+    def working_scope
+      Working.all
+    end
+
+    def service_offering_scope
+      ServiceOffering.friendly
+    end
+
+    def contact_scope
+      Contact.none
     end
   end
 end
