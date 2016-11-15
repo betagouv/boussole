e17bb4af4fc58db0594447e3a66e17530e0f6796
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2016-11-15 11:51:55 +0100

    Filter uptime calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,17 @@ class ApplicationController < ActionController::Base
   # Bug tracking
   before_action :set_raven_context
 
+  # Uptime Robot
+  before_action :return_ok, if: -> { request.user_agent =~ /uptimerobot/i }
+
   private
 
   def set_raven_context
     ::Raven.extra_context(params: params.to_hash, url: request.url)
+  end
+
+  def return_ok
+    return head(:ok)
   end
 
   def features
@@ -29,7 +36,7 @@ class ApplicationController < ActionController::Base
         features,
         request_referer: request.referer,
         request_fullpath: request.fullpath,
-        request_user_agent: request.env['HTTP_USER_AGENT']
+        request_user_agent: request.user_agent
       )
   end
 
--- spec/controllers/visitors_controller_spec.rb
+++ spec/controllers/visitors_controller_spec.rb
@@ -4,10 +4,30 @@
 RSpec.describe VisitorsController, type: :controller do
   let(:event_tracker) { instance_spy(EventTracker) }
 
-  before { allow(subject).to receive(:tracker) { event_tracker } }
+  before do
+    allow(subject).to receive(:tracker) { event_tracker }
+  end
 
-  it 'GET #index' do
-    get(:index)
-    expect(event_tracker).to have_received(:call).with(:jeunes, :visits_landing)
+  describe 'GET #index' do
+    before do
+      request.env['HTTP_USER_AGENT'] = user_agent
+      get(:index)
+    end
+
+    context 'with uptime calls' do
+      let(:user_agent) { 'UptimeRobot' }
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response).not_to render_template(:index) }
+      it { expect(event_tracker).not_to have_received(:call) }
+    end
+
+    context 'without uptime calls' do
+      let(:user_agent) { 'DowntimeRobot' }
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response).to render_template(:index) }
+      it { expect(event_tracker).to have_received(:call).with(:jeunes, :visits_landing) }
+    end
   end
 end
